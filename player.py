cbc0684c6e1658170afb3e4c70746764117cc461
Jimmy <jimmy@bigredgroup.com.au>
2013-09-12 21:15:57 +1000

    More explore

--- player.py
+++ player.py
@@ -80,16 +80,29 @@ class AIPlayer:
 		# Scan in expanding circle outwards
 		distance=1
 		edge=1
-		potentials=[]
-		edges=[1,2,3,4]
-		
 		searchEffort=150
 		pathEffort=4
-		while len(potentials)<=searchEffort:
-			# scan edges
-			for i in edges:
-				for j in xrange(edge):
-					[cx,cy]=self.edgeCalc(i,j,edge,distance)
+		
+		potentials=[]
+		edges=[1,2,3,4]
+		goodPath=False
+		
+		while not goodPath:
+			while len(potentials)<=searchEffort:
+				# scan edges
+				for i in edges:
+					for j in xrange(edge):
+						[cx,cy]=self.edgeCalc(i,j,edge,distance)
+						if cx<0 or cx>=ww: continue
+						if cy<0 or cy>=wh: continue
+						if world.isBlocked(cx,cy) or world.isSeen(cx,cy): continue
+						if world.isPathable(cx,cy):
+							potentials.append((cx,cy))
+							#world.putThing(cx,cy)
+							
+				# and corners
+				for i in edges:
+					[cx,cy]=self.cornerCalc(i,distance,px,py)
 					if cx<0 or cx>=ww: continue
 					if cy<0 or cy>=wh: continue
 					if world.isBlocked(cx,cy) or world.isSeen(cx,cy): continue
@@ -97,58 +110,54 @@ class AIPlayer:
 						potentials.append((cx,cy))
 						#world.putThing(cx,cy)
 						
-			# and corners
-			for i in edges:
-				[cx,cy]=self.cornerCalc(i,distance,px,py)
-				if cx<0 or cx>=ww: continue
-				if cy<0 or cy>=wh: continue
-				if world.isBlocked(cx,cy) or world.isSeen(cx,cy): continue
-				if world.isPathable(cx,cy):
-					potentials.append((cx,cy))
-					#world.putThing(cx,cy)
-					
-			distance=distance+1
-			edge=edge+2
+				distance=distance+1
+				edge=edge+2
+				
+				if distance>max(ww,wh):
+					self.explored=True
+					return (False,False)
 			
-			if distance>max(ww,wh):
-				self.explored=True
-				return (False,False)
-		
-		# now pop random for effort amount and pick shortest path
-		distRank={}
-		for cd in potentials:
-			dst=abs(px-cd[0])+abs(py-cd[1])
-			while dst in distRank: dst=dst+1
-			distRank[dst]=cd
-		
-		potentials=[]
-		print len(distRank),searchEffort
-		for i in xrange(int(searchEffort/2)):
-			potentials.append(distRank[distRank.keys()[i]])
-			#world.putThing(potentials[i][0],potentials[i][1])
+			# now pop random for effort amount and pick shortest path
+			distRank={}
+			for cd in potentials:
+				dst=abs(px-cd[0])+abs(py-cd[1])
+				while dst in distRank: dst=dst+1
+				distRank[dst]=cd
 			
-		pselect=random.sample(potentials,pathEffort)
-		distRank={}
-		if not self.pathMap: self.pathMap=world.getBlockedMap()
-		for cd in pselect:
-			#world.putThing(cd[0],cd[1],"*")
-			path=libtcod.path_new_using_map(self.pathMap,0)
-			libtcod.path_compute(path,self.x,self.y,cd[0],cd[1])
-			ps=libtcod.path_size(path)
-			distRank[ps]=(cd,path)
+			potentials=[]
+			for i in xrange(int(searchEffort/2)):
+				potentials.append(distRank[distRank.keys()[i]])
+				#world.putThing(potentials[i][0],potentials[i][1])
+				
+			pselect=random.sample(potentials,pathEffort)
+			distRank={}
+			if not self.pathMap: self.pathMap=world.getBlockedMap()
+			for cd in pselect:
+				#world.putThing(cd[0],cd[1],"*")
+				path=libtcod.path_new_using_map(self.pathMap,0)
+				libtcod.path_compute(path,self.x,self.y,cd[0],cd[1])
+				ps=libtcod.path_size(path)
+				distRank[ps]=(cd,path)
+			
+			# get closest path len - if it is very high compared to distance, reject and expand search
+			dkKeys=sorted(distRank)
+			if dkKeys[0] < distance*4:
+				ret=distRank[distRank.keys()[0]]
+				goodPath=True
+				print "Successful search at pl",dkKeys[0],"distance",distance,"took",int(math.floor((time.time()-t0)*1000)),"ms"
+			else:
+				print "Rejected search at pl",dkKeys[0],"distance",distance,"took",int(math.floor((time.time()-t0)*1000)),"ms"
 		
-		ret=sorted(distRank[distRank.keys()[0]])
-		print "Edge search took ",time.time()-t0
 		return ret
 	
 	def tick(self,world):
+		if self.explored: return
 		if not self.goal:
-		
 			[self.goal,self.path] = self.findExplorePoint(world)
-			while not world.isPathable(self.goal[0],self.goal[1]): 
-				[self.goal,self.path] = self.findExplorePoint(world)
-				print "reroll"
-			
+			if not self.goal:
+				self.explored=True
+				print "All done"
+				return
 			self.path_progress=0
 		
 		#print self.goal, self.x,self.y,libtcod.path_size(self.path),self.path_progress
